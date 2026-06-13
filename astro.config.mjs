0bc8d0209cf68434d28e8733b6dcd7fec56ace3a
BabaBoi5775 <BabaBoi5775@proton.me>
2026-06-13 17:02:51 +0800

    tuffies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import sitemap from "@astrojs/sitemap"
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   site: "https://pallas-cat-atlas.pages.dev",
   integrations: [sitemap()],
